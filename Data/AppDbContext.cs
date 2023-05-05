0ef530968fbbb02d1967b7d0cc2b554ddb1f96db
Jeú <jeufrancosilva@gmail.com>
2023-05-05 13:59:24 -0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using crud_blazor_server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_blazor_server.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Cliente> Clientes { get; set; } = null!;
+}
